ca94f75cf7e278dfaa4be5dc9011262c1a64d968
Leandro Guido Lorenzini <leandroguidos@gmail.com>
2025-05-07 23:58:02 -0300

    perceptron

--- README.md
+++ README.md
@@ -1,11 +1,30 @@
-# Projeto01CompParalela
+# Implementation of a Neural Network from Scratch in C++
 
-O [dataset](https://www.kaggle.com/datasets/camnugent/california-housing-prices?resource=download) ([housing.csv](housing.csv)) contém informações sobre imóveis na Califórnia com base no censo de 1990. Inclui estatísticas como localização, idade das residências, número de cômodos, população, renda e valor mediano das casas. Os dados usados para o agrupamento foi latitude, longitude e o valor mediano das casas.
+> Only using the C++ STL (standard template library).
 
-## Sequêncial
+## Disclaimer
+
+This was not my idea. I merely followed up on [this great tutorial](https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/), written by Jason Brownlee, where he explains the steps of programming a neural network from scratch in **Python** without the use of any library.
+
+## Details
+
+Porting the python code from Jason Brownlee to c++ is a great exercise to freshen up a bit on object oriented programming (using classes to represent neurons and layers) and vector handling. It also helps a great deal with understanding how neural networks work on the inside. However, there are many ways that a network like this can be build in c++ and I chose to dive into it without much planning. So the code might not be very beautiful, perfectly encapsulated or easy to comprehend (as a matter of fact, I know it isn't😔) but it was fun nevertheless.
+
+## Compilation & Usage
+
+This code was built in VisualStudio (2019 v16.9.4) with `g++ 8.1.0` (using C++14).
+
+You can either import the code in an empty console project in VisualStudio or compile it directly on the command line with:
+
 ```
-python preprocess.py
-g++ -o kmeans.exe kmeans.cpp
-./kmeans.exe
-python show.py
+g++ -Wall -Wpedantic main.cpp NeuralNetwork.cpp
 ```
+Simply running it will read the provided `csv` file from the same directory, train the network and attempt predictions afterwards. Based on the results of the predictions the accuracy is calculated and subsequently printed to `stdout`.
+
+### Result
+
+Using this network on the given seed dataset (and cross-validation) we are able to achieve an approximate accuracy of more than 90%.
+
+The following result was achieved using a learning rate of 0.3 with 500 epochs and 5 neurons in the first hidden layer on a network with one hidden and one output layer.
+
+![Screenshot of example output](ExampleOutput.PNG)
--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,73 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 
-# Incluindo median_house_value na normalização
-home_data = pd.read_csv('housing.csv', usecols=['longitude', 'latitude', 'median_house_value'])
+# Definir os nomes das colunas conforme especificado
+colunas = [
+    'age', 'workclass', 'fnlwgt', 'education', 'education-num',
+    'marital-status', 'occupation', 'relationship', 'race', 'sex',
+    'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income'
+]
 
-pd.DataFrame(home_data, columns=['longitude', 'latitude', 'median_house_value']).to_csv('housing_pre.csv', index=False)
+# Carregar o dataset
+data = pd.read_csv('adults/adult.data', header=None, names=colunas, na_values=' ?')
+
+print(f"Dataset original: {data.shape}")
+
+data = data.dropna()  # Remover linhas com valores ausentes
+print(f"Dataset após remoção de NaN: {data.shape}")
+
+data = data.drop_duplicates()  # Remover duplicatas
+print(f"Dataset após remoção de duplicatas: {data.shape}")
+
+# Separar as variáveis numéricas e categóricas
+numericas = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+categoricas = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country', 'income']
+
+# Inicializar o MinMaxScaler para normalizar as variáveis numéricas
+scaler = MinMaxScaler()
+
+# Normalizar as variáveis numéricas
+data[numericas] = scaler.fit_transform(data[numericas])
+
+# Inicializar o LabelEncoder para codificar as variáveis categóricas
+encoder = LabelEncoder()
+
+# Codificar as variáveis categóricas
+for coluna in categoricas:
+    data[coluna] = encoder.fit_transform(data[coluna].astype(str))
+
+# Salvar o dataset pré-processado em um novo arquivo CSV
+data.to_csv('adult.csv', header=False, index=False)
+
+# ------------------------------ fazer o mesmo para o conjunto de teste
+
+# Carregar o dataset
+data = pd.read_csv('adults/adult.test', header=None, names=colunas, na_values=' ?')
+
+print(f"Dataset original: {data.shape}")
+
+data = data.dropna()  # Remover linhas com valores ausentes
+print(f"Dataset após remoção de NaN: {data.shape}")
+
+data = data.drop_duplicates()  # Remover duplicatas
+print(f"Dataset após remoção de duplicatas: {data.shape}")
+
+# Separar as variáveis numéricas e categóricas
+numericas = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+categoricas = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country', 'income']
+
+# Inicializar o MinMaxScaler para normalizar as variáveis numéricas
+scaler = MinMaxScaler()
+
+# Normalizar as variáveis numéricas
+data[numericas] = scaler.fit_transform(data[numericas])
+
+# Inicializar o LabelEncoder para codificar as variáveis categóricas
+encoder = LabelEncoder()
+
+# Codificar as variáveis categóricas
+for coluna in categoricas:
+    data[coluna] = encoder.fit_transform(data[coluna].astype(str))
+
+# Salvar o dataset pré-processado em um novo arquivo CSV
+data.to_csv('adult_test.csv', header=False, index=False)
